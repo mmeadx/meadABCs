825fc36f0f7a63dd8c3f6d097a6449b278f96292
Adam Mead <amead@avioconsulting.com>
2021-01-31 23:43:50 -0600

    Disallow key activity while sound is playing

--- abc.js
+++ abc.js
@@ -1,23 +1,18 @@
 console.log("abc.js running");
 
+var currentKey = "";
+var soundPlaying = false;
+
 // Click Handler for title "Let's practice our ABCs"
 document.querySelector("h1").addEventListener("click", function () {
     let buttonInnerHTML = this.innerHTML;
     console.log(buttonInnerHTML);
 
-    soundOff(buttonInnerHTML);
-
+    buttonActivity(buttonInnerHTML);
 })
 
 // Get total length of buttons
 var alphabet = document.querySelectorAll(".letter").length;
-
-// Add letters to an object
-function LetterObject(imageName, audioFile, keyboardKey) {
-    this.imageName = imageName;
-    this.audioFile = audioFile;
-    this.keyboardKey = keyboardKey;
-}
 
 // Click Handler | add Event Listeners to each Letter button - then take small letter
 
@@ -27,8 +22,7 @@ for (let i = 0; i < alphabet; i++) {
         let buttonInnerHTML = this.innerHTML[1];
         console.log(buttonInnerHTML);
 
-        buttonAnimation(buttonInnerHTML);
-        soundOff(buttonInnerHTML);
+        buttonActivity(buttonInnerHTML);
     })
 }
 
@@ -38,33 +32,30 @@ addEventListener("keydown", function (event) {
     let keyboardKey = event.key;
     console.log(keyboardKey);
 
-    buttonAnimation(keyboardKey);
-    soundOff(keyboardKey);
-
+    buttonActivity(keyboardKey);
 })
 
-// Sound Function for each letter
-function soundOff(key) {
-    if (key == "Mead ABCs") {
-        let ABCs = new Audio("https://raw.githubusercontent.com/mmeadx/meadABCs/main/sounds/ABCs.m4a");
-        ABCs.play();
-    } else {
-        let letter = new Audio(`https://raw.githubusercontent.com/mmeadx/meadABCs/main/sounds/${key}.m4a`)
-        letter.play();
+function buttonActivity(key) {
+    if (!soundPlaying) {
+        currentKey = key;
+        soundPlaying = true;
+
+        let activeKey = document.querySelector("." + currentKey);
+        activeKey.classList.add(currentKey + "Pressed");
+        if (key === "Mead ABCs") {
+            let ABCs = new Audio("https://raw.githubusercontent.com/mmeadx/meadABCs/main/sounds/ABCs.m4a");
+            ABCs.play().then(buttonActivityComplete, buttonActivityComplete);
+        } else {
+            let letter = new Audio(`https://raw.githubusercontent.com/mmeadx/meadABCs/main/sounds/${key}.m4a`)
+            letter.play().then(buttonActivityComplete, buttonActivityComplete);
+            ;
+        }
     }
 }
 
-// This removes the text and adds a photo for 3 seconds
-
-function buttonAnimation(currentKey) {
-
-    console.log("button animation running on " + currentKey);
-
+function buttonActivityComplete() {
     let activeKey = document.querySelector("." + currentKey);
-
-    activeKey.classList.add(currentKey + "Pressed");
-
-    setTimeout(function () {
-        activeKey.classList.remove(currentKey + "Pressed");
-    }, 3000);
+    activeKey.classList.remove(currentKey + "Pressed")
+    soundPlaying = false
+    currentKey = ""
 }
